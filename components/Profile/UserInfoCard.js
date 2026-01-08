b51c336dd0c2189cd0ccb10325df9e759bcdab86
Aidhandy <aidhandy.inc@gmail.com>
2026-01-08 18:54:27 -0500

    Update UserInfoCard.js

--- components/Profile/UserInfoCard.js
+++ components/Profile/UserInfoCard.js
@@ -7,9 +7,9 @@ import { useEffect } from "react";
 
 export default function UserInfoCard({ profile }) {
   const supabase = createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  );
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+);
   const router = useRouter();
 
   useEffect(() => {
